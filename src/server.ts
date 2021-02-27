68b88ef63c2d703f3813a3613a9f194e08373ebb
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-02-27 03:51:14 -0300

    finish

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
 
 app.listen(3000, () => console.log('success'))
